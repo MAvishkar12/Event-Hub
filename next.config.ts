09bd281de9c9ce14766acdb55bce44be96d9860c
MAvishkar12 <moreavishkar12@gamil.com>
2026-03-08 17:13:51 +0530

    handle remote image in next.config

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+        {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      }
     ],
   },
   /* config options here */
